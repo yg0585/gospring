cace023af0c095dce5ff07f28ab8f3ded41efc09
yg0585 <yg0585@naver.com>
2021-10-24 19:58:31 +0900

    Initial commit

---
+++ gospring/src/main/java/com/example/gospring/GospringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GospringApplication.class, args);
+	}
+
+}
